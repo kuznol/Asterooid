22b8289225d1f4c79de015c74d9a1975f77e5b15
Olesya Cuznetsova <olesya.cuznetsova2011@yandex.ru>
2020-02-21 18:18:41 +0300

    игроки...

--- Asterooid/Program.cs
+++ Asterooid/Program.cs
@@ -14,7 +14,8 @@ namespace Asterooid
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            
+Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
