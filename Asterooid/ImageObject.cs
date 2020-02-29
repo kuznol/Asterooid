dc5d763813941c2e554ac248fcb9eab536558858
Olesya Cuznetsova <olesya.cuznetsova2011@yandex.ru>
2020-02-29 10:14:19 +0300

    Астероид

---
+++ Asterooid/ImageObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asterooid
+{
+     class ImageObject : VisualObject
+    {
+        private Image Image;
+        public ImageObject(Point Position, Point Direction, Size Size, Image Image)
+            : base(Position, Direction, Size)
+        {
+            Image = Image;
+        }
+    }
+}
